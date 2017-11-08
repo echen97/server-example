280b1f4494b14add68eaa5878e708d166ceca719
Ethan Chen <ethan@Ethans-MacBook-Pro.local>
2017-11-08 15:16:21 -0500

    uncommented mongoDB stuff

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 // Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
-/*var mongoUri = process.env.MONGOLAB_URI ||
+var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/whereintheworld'; // comp20 is the name of the database we are using in MongoDB
 var mongo = require('mongodb');
 var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
   db = databaseConnection;
-});*/
+});
 
 app.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
